8b934c621915f69036fbdb073d8b9abec011b2bc
Sarah Murdock <62572217+sarah-is-coding@users.noreply.github.com>
2023-08-02 12:24:51 -0400

    tsparticles
    added tsparticles and removed particles.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,63 @@
 import React, { useState, useEffect } from 'react';
-import Particles from 'particles.js';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { tsParticles } from 'tsparticles';
+import About from './about';
+import Portfolio from './portfolio';
+import Contact from './contact';
 import '../styles/styles.scss';
 
 const App = () => {
   // State for Theme Toggle
   const [theme, setTheme] = useState('light');
 
-  // Initialize particles.js in useEffect hook
+  // Initialize tsParticles in useEffect hook
   useEffect(() => {
-    Particles.init({
-      selector: '.particle-js',
-      maxParticles: 100,
-      color: theme === 'light' ? '#000000' : '#ffffff',
+    tsParticles.load("tsparticles", {
+      particles: {
+        number: {
+          value: 80,
+          density: {
+            enable: true,
+            area: 800
+          }
+        },
+        color: {
+          value: theme === 'light' ? '#000000' : '#ffffff'
+        },
+        shape: {
+          type: "image",
+          image: {
+            src: "../../assets/images/cherry_blossom.png",
+            width: 100,
+            height: 100
+          }
+        },
+        opacity: {
+          value: 0.5,
+          random: false
+        },
+        size: {
+          value: 5,
+          random: true
+        },
+        move: {
+          enable: true,
+          speed: 1,
+          direction: "bottom",
+          random: false,
+          straight: false,
+          outMode: "out",
+          bounce: false
+        }
+      },
+      interactivity: {
+        events: {
+          onHover: {
+            enable: true,
+            mode: "repulse"
+          }
+        }
+      }
     });
   }, [theme]);
 
@@ -23,25 +69,40 @@ const App = () => {
   };
 
   return (
-    <div className="app">
-      <header className="font-josefin">
-        <h1>My Portfolio</h1>
-        <button onClick={toggleTheme} className="theme-toggle">
-          {theme === 'light' ? 'Dark' : 'Light'} Theme
-        </button>
-      </header>
-      <main className="font-playfair">
-        <section>
-          <h2 className="hover:scale-105">About Me</h2>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo, harum?</p>
-        </section>
-        <section>
-          <h2 className="hover:scale-105">Projects</h2>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo, harum?</p>
-        </section>
-      </main>
-      <div className="particles-js"></div>
-    </div>
+    <Router>
+      <div className="app">
+        <header className="font-josefin">
+          <nav>
+            <ul className="flex justify-between p-4">
+              <li className="hover:scale-105 transition-transform duration-300">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="hover:scale-105 transition-transform duration-300">
+                <Link to="/about">About</Link>
+              </li>
+              <li className="hover:scale-105 transition-transform duration-300">
+                <Link to="/portfolio">Portfolio</Link>
+              </li>
+              <li className="hover:scale-105 transition-transform duration-300">
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li className="dark-light-toggle hover:scale-105 transition-transform duration-300">
+                <button onClick={toggleTheme}>
+                  {theme === 'light' ? 'Dark' : 'Light'} Theme
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <div id="tsparticles" className="particles-js"></div>
+      </div>
+    </Router>
   );
 }
 

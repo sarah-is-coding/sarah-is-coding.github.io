28da1297ba78dbdf8939f126c78203aa691269c7
Sarah Murdock <sg.mdoc7@gmail.com>
2024-06-03 19:18:51 -0400

    cf2

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -11,7 +11,7 @@ const ContactForm = () => {
 		e.preventDefault();
 
 		emailjs
-			.sendForm("h", "h", e.target, "h")
+			.sendForm("service_yt0403j", "template_4ib670w", e.target, "sg.mdoc7")
 			.then(
 				(result) => {
 					document.getElementById("contact_form").reset();
